ac6d78bda2f0dfdeccf221f7b37a65c9e3f41ee9
kimJeongSeong <wjdtjd9988@gamil.com>
2026-02-25 15:34:51 +0900

    fix: vite config base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/weather_project_/' ,
+  base: '/Weather_project/' ,
 })
